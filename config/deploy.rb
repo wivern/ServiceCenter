9660a0bc7f94aa9684cb0de15efb99076d2655e1
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2011-12-27 17:03:05 +0400

    add deploy receipt to support shared resources

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 set :application, "service_center"
 
-$LOAD_PATH.unshift File.expand_path('./lib',ENV['rvm_path'])
+$LOAD_PATH.unshift File.expand_path('./lib', ENV['rvm_path'])
 require 'rvm/capistrano'
 require 'bundler/capistrano'
-load File.join(File.dirname(__FILE__),"deploy/targets.rb")
+load File.join(File.dirname(__FILE__), "deploy/targets.rb")
 
 set :rvm_ruby_string, '1.9.2'
 set :rvm_type, :user
 
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-set :repository, "git@github.com:wivern/ServiceCenter.git"  # Your clone URL
+default_run_options[:pty] = true # Must be set for the password prompt from git to work
+set :repository, "git@github.com:wivern/ServiceCenter.git" # Your clone URL
 set :scm, "git"
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+                                 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :branch, "master"
 set :use_sudo, false
@@ -30,9 +30,30 @@ set :deploy_via, :remote_cache
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+
+  after "deploy:setup", "deploy:resources:setup"
+
+  task :start do
+    ;
+  end
+  task :stop do
+    ;
+  end
+  task :restart, :roles => :app, :except => {:no_release => true} do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+
+  namespace :resources do
+    desc "create shared resources folder"
+    task :setup do
+      run "#{try_sudo} cd #{shared_path}/system; mkdir assets; mkdir icons; mkdir javascripts"
+    end
+
+    desc "makes symbolic links to shared"
+    task :symlinks do
+      run "#{try_sudo} cd #{current_path}/public; ln -s #{shared_path}/system/assets assets"
+      run "#{try_sudo} cd #{current_path}/public; ln -s #{shared_path}/system/images/icons images/icons"
+      run "#{try_sudo} cd #{current_path}/public; ln -s #{shared_path}/system/javascripts/ext-4.0.7-gpl extjs"
+    end
+  end
 end
