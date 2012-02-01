15c8957352d7c7df6d24a2479e18402883030693
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2012-02-01 11:17:18 +0400

    bug when roles is Nil

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,22 +1,25 @@
 class Position < ActiveRecord::Base
   netzke_exclude_attributes :created_at, :updated_at, :roles
   validates_presence_of :name
-  serialize :roles, Array
+  serialize :roles #, Array
 
   attr_accessible :name, :roles
 
   cattr_reader :available_roles
   @@available_roles = {
-      :inspector => { :name => I18n.t('roles.inspector') },
-      :engineer  => { :name => I18n.t('roles.engineer') },
-      :administrator => { :name => I18n.t('roles.administrator') },
-      :manager => { :name => I18n.t('roles.manager') },
-      :director => { :name => I18n.t('roles.director') }
+      :inspector => {:name => I18n.t('roles.inspector')},
+      :engineer => {:name => I18n.t('roles.engineer')},
+      :administrator => {:name => I18n.t('roles.administrator')},
+      :manager => {:name => I18n.t('roles.manager')},
+      :director => {:name => I18n.t('roles.director')}
   }
 
   def roles_names
-    self.roles.select{|e| e}.
-        map{|r| available_roles[r.to_sym][:name] if r}.sort_by{|e| e}.join(', ')
+    self.roles.select{|e|
+      e && self.class.available_roles.has_key?(e.to_sym) }.map{|r|
+        logger.debug "role: #{r}, available: #{self.class.available_roles.inspect}"
+        self.class.available_roles[r.to_sym][:name] if r
+      }.sort_by { |e| e }.join(', ') if self.roles
   end
 
   def roles
