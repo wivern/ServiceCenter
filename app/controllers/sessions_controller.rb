c60231384aa98317597b60319c454f86eb26d6b0
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2012-04-23 14:06:49 +0400

    wsc-3790: Сброс пароля для пользователя

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < Devise::SessionsController
   respond_to :html
   def destroy
     super
+    Netzke::Core.reset_components_in_session
     reset_session
     logger.debug "Session destroyed"
   end
