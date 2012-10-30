9ac27217baf48b89a14c153a762f10c508b8b4c9
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2012-10-30 13:35:22 +0400

    wsc-4232 Сделать разделение цен на з/ч и работы по организациям

--- app/models/order.rb
+++ app/models/order.rb
@@ -86,7 +86,7 @@ class Order < ActiveRecord::Base
   end
 
   def activities_amount
-    working_activities.inject(0){|sum, a| sum + a.price.value}
+    working_activities.inject(0){|sum, a| sum + a.prices.for_organization(organization).value}
   end
 
   def activities_score
