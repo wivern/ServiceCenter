250c249f03c905fae0a87b761ebd846d200f7f0b
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2012-11-07 17:49:26 +0400

    wsc-4215 Иерархический справочник Работ

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -21,9 +21,10 @@ class ExportController < ApplicationController
         :from => @from,
         :till => @till },
       :orders => @orders
-    }.to_xml( :root => :export, :include => {:order_activities => {:include => {:activity => {:except => :id}}, :except => [:order_id, :id, :activity_id]},
+    }.to_xml( :root => :export, :include => {:order_activities => {:include => {:activity => {:except => :id}},
+                                                                   :except => [:order_id, :id, :activity_id]},
                   :order_spare_parts => {:include => {:spare_part => {}}, :except => [:order_id, :id, :updated_at]},
-                  :customer => {}, :repair_type => {}},
+                  :customer => {}, :repair_type => {}, :status => {}},
                   :methods => [:activities_amount, :spare_parts_amount, :total_amount, :total_amount_with_discount],
         :except => [:created_at, :updated_at])
   end
